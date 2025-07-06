3d4f6b70134256458913e36e51d0d2eb17d43159
Nepaletka <ttt978615@gmail.com>
2025-07-06 20:20:51 +0500

    heap_inspect

--- pwninit-template.py
+++ pwninit-template.py
@@ -1,15 +1,21 @@
 from pwn import *
+import os
+
+def debug(p):
+	pid = util.proc.pidof(p)[0]
+	print(pid)
+	util.proc.wait_for_debugger(pid)
+
+def hi(p):
+	os.system("python3 ~/Tools/heapinspect/HeapInspect.py %d", p.pid)
+
 
 context.binary = binary = ELF('{bin_name}_patched', checksec=False)
 context.log_level = "debug"
 
 p = process()
-p = remote("host", port)
+#p = remote("host", port)
 
-#debug
-pid = util.proc.pidof(p)[0]
-print(pid)
-util.proc.wait_for_debugger(pid)
 
 
 p.interactive()

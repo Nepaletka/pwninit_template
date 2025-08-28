49762ebb513a7c5362f9fe1de20027b4bff7e629
Nepaletka <ttt978615@gmail.com>
2025-08-28 21:01:56 +0500

    shortcuts + terminal

--- libs/debug.py
+++ libs/debug.py
@@ -5,3 +5,16 @@ def debug(p):
 	print(pid)
 	util.proc.wait_for_debugger(pid)
 
+
+# find PIE adresses
+#p = process(binary.path)        
+# for i in range (1,60):    
+#     payload = f"%{i}$p".encode()
+#     p.sendlineafter(b'exit',payload)            
+#     try:
+#         p.recvuntil(b"here is your choice\n")
+#         recv=p.recvline().strip()
+#         # if b'0x7' in recv:
+#         warn (f"PAYLOAD: {payload} RECV: {recv}")
+#     except:
+#         pass
--- pwninit-templates/heap_template.py
+++ pwninit-templates/heap_template.py
@@ -2,8 +2,16 @@ from pwn import *
 from libs.debug import *
 from libs.heap import *
 
+#https://agrohacksstuff.io/posts/pwntools-tricks-and-examples/
+
 context.binary = binary = ELF('{bin_name}_patched', checksec=False)
+context.terminal = ['gnome-terminal', '--'])
 context.log_level = "debug"
+
+sl = lambda x: p.sendline(x)
+sa = lambda x, y: p.sendafter(x, y)
+sla = lambda x, y: p.sendlineafter(x, y)
+ru = lambda x: p.recvuntil(x)
 
 p = process()
 #p = remote("host", port)
